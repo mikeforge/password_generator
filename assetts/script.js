e2d4e8e28a44def6bb58d8b3dcb2c25bdda9c432
Mike Kelly <mike.forge@gmail.com>
2021-11-11 23:03:10 -0500

    finished getting random gen function to work with various array assignments

--- assetts/script.js
+++ assetts/script.js
@@ -20,16 +20,16 @@ function asciiArray(low,high) {
 }
 
 //generate the password with info from prompts
-// function checkLength() {
-  // var passopts = { 
-  //   length: Number(prompt("How many characters should your password be? Choose between 8 and 128", "14")),
-  //   lowercase: prompt("Would you like to use lowercase Letters?","yes"),
-  //   uppercase: prompt("Would you like to use Capital Letters?","yes"),
-  //   numeric: prompt("Would you like to use Numeric Values?","yes"),
-  //   special: prompt("Would you like to use Special Characters?","yes")
-  // }
 
-  // //Checking to ensure the number entered is within allowable range.
+function generatePassword() {
+  var passopts = { 
+    length: Number(prompt("How many characters should your password be? Choose between 8 and 128", "14")),
+    lowercase: prompt("Would you like to use lowercase Letters?","yes"),
+    uppercase: prompt("Would you like to use Capital Letters?","yes"),
+    numeric: prompt("Would you like to use Numeric Values?","yes"),
+    special: prompt("Would you like to use Special Characters?","yes")
+  };
+  //Checking to ensure the number entered is within allowable range.
   // var passlength = [];
   //   for (var i = 8; i <=128; i++) {
   //     passlength.push(i);
@@ -41,47 +41,25 @@ function asciiArray(low,high) {
   //       alert("please choose a number between 8 and 128");
   //       console.log("Please choose a number between 8 and 128");
   //    }
-//  }
-
-  let generatePassword = (
-    passopts.length,
-    passopts.lowercase,
-    passopts.uppercase,
-    passopts.numeric,
-    passopts.special
-  ) => {
-    // var passopts = { 
-    //   length: Number(prompt("How many characters should your password be? Choose between 8 and 128", "14")),
-    //   lowercase: prompt("Would you like to use lowercase Letters?","yes"),
-    //   uppercase: prompt("Would you like to use Capital Letters?","yes"),
-    //   numeric: prompt("Would you like to use Numeric Values?","yes"),
-    //   special: prompt("Would you like to use Special Characters?","yes")
-    // }
-    // //Checking to ensure the number entered is within allowable range.
-    // var passlength = [];
-    //   for (var i = 8; i <=128; i++) {
-    //     passlength.push(i);
-    //   }
-  
-    //   if (passlength.indexOf(passopts.length) >= 0) {
-    //       console.log("Length was selected appropriately");
-    //   } else {
-    //       alert("please choose a number between 8 and 128");
-    //       console.log("Please choose a number between 8 and 128");
-    //    }
-    let charCodes = null;
+    // passopts.length,
+    // passopts.lowercase,
+    // passopts.uppercase,
+    // passopts.numeric,
+    // passopts.special
+       
+    let charCodes = lowercase_codes;
     if (passopts.lowercase) charCodes = charCodes.concat(lowercase_codes);
     if (passopts.uppercase) charCodes = charCodes.concat(uppercase_codes);
     if (passopts.numeric) charCodes = charCodes.concat(numeric_codes);
     if (passopts.special) charCodes = charCodes.concat(special_codes);
     const passChars = [];
-    for (let i = 0; i < characterAmount; i++) {
+    for (let i = 0; i < passopts.length; i++) {
       const characterCode =
       charCodes[Math.floor(Math.random() * charCodes.length)];
       passChars.push(String.fromCharCode(characterCode));
     }
     return passChars.join("");
-  };
+};
 
 // Write password to the #password input
 function writePassword() {
@@ -92,7 +70,5 @@ function writePassword() {
 
 }
 
-
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
- 
