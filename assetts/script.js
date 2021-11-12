6955c85603c581f9a97aa7d5f371b414827f9d96
Mike Kelly <mike.forge@gmail.com>
2021-11-11 21:16:29 -0500

    moved variable assignment to within function

--- assetts/script.js
+++ assetts/script.js
@@ -1,46 +1,68 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
  
-// prompting for char requirement options
-var charopts = {
-  length: Number(prompt("How many characters should your password be? Choose between 8 and 128", "14")),
-  lowercase: prompt("Would you like to use lowercase Letters?","yes"),
-  Uppercase: prompt("Would you like to use Capital Letters?","yes"),
-  Numeric: prompt("Would you like to use Numeric Values?","yes"),
-  Special: prompt("Would you like to use Special Characters?","yes")
+// prompting for pass requirement options
+// 
 
-}
 
-// Setting contstraint check for length prompt
-var passlength = [];
+// var passopts = { //Look at moving this local to scope of addEventListener at bottom...
+//   length: Number(prompt("How many characters should your password be? Choose between 8 and 128", "14")),
+//   lowercase: prompt("Would you like to use lowercase Letters?","yes"),
+//   Uppercase: prompt("Would you like to use Capital Letters?","yes"),
+//   Numeric: prompt("Would you like to use Numeric Values?","yes"),
+//   Special: prompt("Would you like to use Special Characters?","yes")
+
+// }
+
+// Setting contstraint check for length prompt  //Moved this to within scope of function as well to only fire after btn click.
+// var passlength = [];
+//     for (var i = 8; i <=128; i++) {
+//       passlength.push(i);
+//     }
+
+//     if (passlength.indexOf(passopts.length) >= 0) {
+//         console.log("Length was selected appropriately");
+//     } else {
+//         console.log("Please choose a number between 8 and 128");
+//     }
+
+
+// Write password to the #password input
+function writePassword() {
+  var passopts = { //Look at moving this local to scope of generate function instead...
+    length: Number(prompt("How many characters should your password be? Choose between 8 and 128", "14")),
+    lowercase: prompt("Would you like to use lowercase Letters?","yes"),
+    Uppercase: prompt("Would you like to use Capital Letters?","yes"),
+    Numeric: prompt("Would you like to use Numeric Values?","yes"),
+    Special: prompt("Would you like to use Special Characters?","yes")
+  }
+
+  //Checking to ensure the number entered is within allowable range.
+  var passlength = [];
     for (var i = 8; i <=128; i++) {
       passlength.push(i);
     }
 
-    if (passlength.indexOf(charopts.length) >= 0) {
+    if (passlength.indexOf(passopts.length) >= 0) {
         console.log("Length was selected appropriately");
     } else {
         console.log("Please choose a number between 8 and 128");
     }
 
-
-
-
-
-
-
-
-
-
-
-// Write password to the #password input
-function writePassword() {
+  //calling the function to actually print the password
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
 
 }
+
+function generatePassword() {
+  if (passopts.lowercase = true) {
+
+  }
+}
+
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
